7b6f49845c8d4135f06d820070a09fe77308d007
Walid <Walid@example.com>
2025-12-01 17:32:20 +0100

    Exercice Py

--- Exam 3.py
+++ Exam 3.py
@@ -1,3 +1,31 @@
-s=input("Entrez le chiffre Romain:")
-s=
-if()
+def ChiffreRomain(n):
+    valeurs = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    i = 0
+    while i < len(n):
+        
+        valeur_n1 = valeurs[n[i]]
+
+        if i + 1 < len(n):
+            valeur_n2 = valeurs[n[i + 1]]
+
+            if valeur_n1 < valeur_n2:
+                total += valeur_n1 - valeur_n2
+                i += 2
+                continue
+
+        total += valeur_n1
+        i += 1
+
+    return total
+
+print(ChiffreRomain("XXVII"))
